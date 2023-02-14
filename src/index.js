83b2427b17ae080039c44c3fb0bd4e303ff98975
marcus featherston <marcusovfeatherston@gmail.com>
2023-02-14 11:26:31 -0700

    made the fetch to get releases data from API

--- src/index.js
+++ src/index.js
@@ -2,21 +2,50 @@ const form = document.querySelector("form");
 
 const url = "https://api.discogs.com";
 
-// const releasesUrl = `https://api.discogs.com/artists/{${artistId}}/releases`;
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const searchedArtist = event.target.artistSearch.value;
-  console.log(event.target.artistSearch.value);
+  // console.log(event.target.artistSearch.value);
   fetch(`${url}/database/search?q={${searchedArtist}}`, {
     headers: {
       Authorization: TOKEN,
     },
   })
     .then((response) => response.json())
-    .then((data) => {
-      const artistId = data.results[0].id;
-      console.log(artistId);
+    .then((artistList) => {
+      const artistId = artistList.results[0].id;
+      fetch(`${url}/artists/${artistId}/releases`, {
+        headers: {
+          Authorization: TOKEN,
+        },
+      })
+        .then((response) => response.json())
+        .then((albumList) => {
+          albumList.releases.forEach((album) => {
+            //create elements to append to the DOM
+          });
+        });
     });
 });
 //
+// {
+//     "id": 2328,
+//     "title": "The GrimmRobe Demos",
+//     "type": "master",
+//     "main_release": 509475,
+//     "artist": "Sunn O)))",
+//     "role": "Main",
+//     "resource_url": "https://api.discogs.com/masters/2328",
+//     "year": 2000,
+//     "thumb": "https://i.discogs.com/8db6cAkt_8b3vrgbDdJcMmqtQ876VVWUMFVdjIkqoyo/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTUwOTQ3/NS0xMTY3NDMyOTEz/LmpwZWc.jpeg",
+//     "stats": {
+//         "community": {
+//             "in_wantlist": 156,
+//             "in_collection": 197
+//         },
+//         "user": {
+//             "in_wantlist": 0,
+//             "in_collection": 0
+//         }
+//     }
+// }
