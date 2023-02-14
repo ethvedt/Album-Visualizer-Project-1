5d02fc444915cebe1a2544090dd09f781d781842
marcus featherston <marcusovfeatherston@gmail.com>
2023-02-14 08:59:49 -0700

    added event listener to the artist search input form and built fetch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-// hard coded search for now to make sure auth was working
-fetch("https://api.discogs.com/database/search?q={prince}&{?artist}", {
-  headers: {
-    Authorization: TOKEN,
-  },
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+const form = document.querySelector("form");
+
+const url = "https://api.discogs.com";
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const searchedArtist = event.target.artistSearch.value;
+  console.log(event.target.artistSearch.value);
+  fetch(`${url}/database/search?q={${searchedArtist}}`, {
+    headers: {
+      Authorization: TOKEN,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const artistId = data.results[0].id;
+      console.log(artistId);
+    });
+});
